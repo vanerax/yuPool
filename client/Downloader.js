51c0fe88f5575e8637d237a763937b18e0f3780a
Ricky Zhuang <ricky.zhuang@oracle.com>
2017-05-05 17:43:41 +0800

    test write stream and pipe

--- client/Downloader.js
+++ client/Downloader.js
@@ -1,5 +1,6 @@
 const url = require('url');
 const http = require('http');
+const https = require('https');
 const fs = require('fs');
 const BASE_PATH = "e:\\temp";
 
@@ -8,8 +9,11 @@ function Downloader() {
 
 }
 
-Downloader.prototype.run = function(sUrl, sFullPathName) {
-	var oUrl = url.parse(sUrl);
+Downloader.prototype.run = function(sUrl, sFilename) {
+    if (!sFilename) {
+        sFilename = _generateFullPathName();
+    }
+    var oUrl = url.parse(sUrl);
     var oOption = {
         hostname: oUrl.hostname,
         //port: 80,
@@ -20,42 +24,63 @@ Downloader.prototype.run = function(sUrl, sFullPathName) {
 
     http.request(oOption, function(res){
         //assert.ok(res instanceof http.IncomingMessage, "IncomingMessage");
-        var fd = fs.openSync(_generateFullPathName(), "w");
+        var fd = fs.openSync(sFilename, "w");
         let aPool = [];
         let nSize = 0;
         let nTotalSize = 0;
 
         res.on('data', function(chunk){
-        	nSize += chunk.length;
-        	nTotalSize += chunk.length;
-        	aPool.push(chunk);
-        	console.log(".");
+            nSize += chunk.length;
+            nTotalSize += chunk.length;
+            aPool.push(chunk);
+            console.log(".");
 
-        	fs.writeSync(fd, "start\n");
-        	if (nSize > 1024 * 1024) {
-        		var buff = Buffer.concat(aPool);
-        		fs.writeSync(fd, buff, 0, buff.length);
-        		aPool = [];
-        		nSize = 0;
-        		console.log(`total size: ${nTotalSize}`);
-        	}
+            fs.writeSync(fd, "start\n");
+            if (nSize > 1024 * 1024) {
+                var buff = Buffer.concat(aPool);
+                fs.writeSync(fd, buff, 0, buff.length);
+                aPool = [];
+                nSize = 0;
+                console.log(`total size: ${nTotalSize}`);
+            }
         });
         res.on('end', function(){
             var buff = Buffer.concat(aPool);
             //console.log(buff);
-        	fs.writeSync(fd, buff, 0, buff.length);
+            fs.writeSync(fd, buff, 0, buff.length);
             fs.close(fd);
         });
     }).end(); // return <http.ClientRequest>
 };
 
+Downloader.prototype.runWriteStream = function(sUrl, oWriteStream) {
+    var oUrl = url.parse(sUrl);
+    var oOption = {
+        hostname: oUrl.hostname,
+        //port: 80,
+        path: oUrl.path,
+        method: 'GET',
+        headers: {}
+    };
+
+    https.request(oOption, function(res){
+        //assert.ok(res instanceof http.IncomingMessage, "IncomingMessage");
+        let aPool = [];
+        let nSize = 0;
+        let nTotalSize = 0;
+        res.pipe(oWriteStream);
+
+    }).end(); // return <http.ClientRequest>
+};
+
 function _generateFullPathName() {
-	var ts = Math.floor(new Date().getTime() / 1000);
-	var filename = BASE_PATH + "\\" + ts + ".flv";
-	return filename;
+    var ts = Math.floor(new Date().getTime() / 1000);
+    var filename = BASE_PATH + "\\" + ts + ".flv";
+    return filename;
 }
 
 module.exports = Downloader;
-
+var fws = fs.createWriteStream('z2.txt');
 var d = new Downloader();
-d.run("http://www.baidu.com/");
+
+d.runWriteStream("https://confluence.oraclecorp.com/", fws);
--- client/test1.js
+++ client/test1.js
@@ -1,10 +1,63 @@
 const douyu = require('./douyu');
 const SMPlayer = require('./SMPlayer');
 const constant = require('../common/constant');
+const fs =require('fs');
+const Writable = require('stream').Writable;
 
-var sPayload = 
-    "did=9C9B29EE0C9EA895F06C92FDADC800ED&cdn=ws&ver=2017040801&sign=10440fcf0baefcaf156dad0611d37e96&tt=24864006&rate=2";
-douyu.getPlay(210486, sPayload, function(sUrl){
-    var player = new SMPlayer();
-    player.play(sUrl);
-})
+class MyWritable extends Writable {
+  constructor(options) {
+    super(options);
+  }
+
+  _write(chunk, encoding, callback) {
+    if (chunk.toString().indexOf('a') >= 0) {
+      callback(new Error('chunk is invalid'));
+    } else {
+      callback();
+    }
+  }
+}
+
+var mw = new MyWritable();
+console.log(MyWritable);
+
+mw.write("hello world", function(){
+    console.log('written for my writeable');
+    console.log(arguments);
+});
+
+var ws = new fs.createWriteStream('z.txt');
+ws.on('open', function(){
+    console.log('open');
+    console.log(arguments);
+});
+ws.on('close', function(){
+    console.log('close');
+    console.log(arguments);
+});
+
+ws.on('finish', function(){
+    console.log('finish');
+    console.log(arguments);
+});
+
+ws.on('error', function(){
+    console.log('error');
+    console.log(arguments);
+});
+
+var bRet = ws.write("test1", function(){
+    console.log('written1');
+    console.log(arguments);
+});
+console.log(bRet);
+
+bRet = ws.write("test2", function(){
+    console.log('written2');
+    console.log(arguments);
+});
+console.log(bRet);
+
+//process.stdin.pipe(ws);
+
+ws.end();
