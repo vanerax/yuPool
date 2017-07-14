5247b3183f06f9e5d337d6bdd8f694de750cf68c
zhuanr <vanerax@gmail.com>
2017-07-14 23:48:46 +0800

    add cmd wrapper

---
+++ chenyifa.cmd
@@ -0,0 +1,5 @@
+cd xClient
+start node app.js 67373 %*
+cd ..\client
+start node danmu2.js 67373 %*
+cd ..
---
+++ feifei.cmd
@@ -0,0 +1,5 @@
+cd xClient
+start node app.js 2089340 %*
+cd ..\client
+start node danmu2.js 2089340 %*
+cd ..
